ef36c9e50452dd3ff34be6096cb80bc9412b6355
nataliamarth <98548283+nataliamarth@users.noreply.github.com>
2024-01-28 16:55:58 +0100

    Run tests headless

--- RunTests.bat
+++ RunTests.bat
@@ -1,4 +1,4 @@
 "C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%CD%\DevOpsGame.uproject" ^
- -nosplash -stdout -Unattended -nopause -nosound -nocontentbrowser -log -ExecCmds="Automation RunTests Project.Functional Tests; Quit" ^
+ -nosplash -stdout -Unattended -nopause -nosound -nocontentbrowser -log -NullRHI -ExecCmds="Automation RunTests Project.Functional Tests; Quit" ^
      -TestExit="Automation Test Queue Empty"  ^
     -ReportExportPath="%CD%\Reports" -Log 
