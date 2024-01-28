84de42ffadb893c12426f423bd072a1fec0b54b3
nataliamarth <98548283+nataliamarth@users.noreply.github.com>
2024-01-28 16:40:16 +0100

    Update UE CMD path
    Use absoloute defualt UE 5 path for now.

--- RunTests.bat
+++ RunTests.bat
@@ -1,4 +1,4 @@
-UnrealEditor-Cmd.exe "%CD%\DevOpsGame.uproject" ^
+"C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "%CD%\DevOpsGame.uproject" ^
  -nosplash -stdout -Unattended -nopause -nosound -nocontentbrowser -log -ExecCmds="Automation RunTests Project.Functional Tests; Quit" ^
      -TestExit="Automation Test Queue Empty"  ^
     -ReportExportPath="%CD%\Reports" -Log 
